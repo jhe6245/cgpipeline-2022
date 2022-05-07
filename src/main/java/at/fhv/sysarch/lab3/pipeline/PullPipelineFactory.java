b7547073c9ccd3a0b07c78df17e8ccf2f341bea7
jhe6245 <jhe6245@students.fhv.at>
2022-05-07 21:20:31 +0200

    cleanup

--- src/main/java/at/fhv/sysarch/lab3/pipeline/PullPipelineFactory.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/PullPipelineFactory.java
@@ -4,39 +4,33 @@ import at.fhv.sysarch.lab3.animation.AnimationRenderer;
 import at.fhv.sysarch.lab3.obj.Face;
 import at.fhv.sysarch.lab3.obj.Model;
 import at.fhv.sysarch.lab3.pipeline.pull.*;
-import at.fhv.sysarch.lab3.utils.MatrixUtils;
 import com.hackoeur.jglm.Mat4;
 import com.hackoeur.jglm.Matrices;
 import com.hackoeur.jglm.Vec4;
 import javafx.animation.AnimationTimer;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class PullPipelineFactory {
 
-    private static Mat4 modelToViewTransform(PipelineData pd, float rotationAngle) {
+    private static Mat4 modelSpaceToViewSpace(PipelineData pd, float rotationAngle) {
         return pd.getModelTranslation()
                 .multiply(pd.getViewTransform())
                 .multiply(Matrices.rotate(rotationAngle, pd.getModelRotAxis()));
     }
 
     public static AnimationTimer createPipeline(PipelineData pd) {
-        // TODO: pull from the source (model)
-
-        final Model[] inputModel = {null};
 
         var source = new Source<Model>() {
-
             @Override
             public Model pull() {
-                return inputModel[0];
+                return pd.getModel();
             }
         };
 
-        // TODO 1. perform model-view transformation from model to VIEW SPACE coordinates
-
-        var filterViewTransform = new TransformFilter(new Pipe<>(source), modelToViewTransform(pd, 0));
+        var filterViewTransform = new TransformFilter(new Pipe<>(source), modelSpaceToViewSpace(pd, 0));
 
         // TODO 2. perform backface culling in VIEW SPACE
 
@@ -47,15 +41,10 @@ public class PullPipelineFactory {
         // lighting can be switched on/off
         if (pd.isPerformLighting()) {
             // 4a. TODO perform lighting in VIEW SPACE
-            
-            // 5. TODO perform projection transformation on VIEW SPACE coordinates
-        } else {
-            // 5. TODO perform projection transformation
         }
 
         var filterProjTransform = new TransformFilter(new Pipe<>(filterViewTransform), pd.getProjTransform());
 
-        // TODO 6. perform perspective division to screen coordinates
         var filterPerspectiveDivision = new PerFaceFilter(new Pipe<>(filterProjTransform)) {
 
             Vec4 div(Vec4 v) {
@@ -73,8 +62,6 @@ public class PullPipelineFactory {
 
         var filterViewportTransform = new TransformFilter(new Pipe<>(filterPerspectiveDivision), pd.getViewportTransform());
 
-        // TODO 7. feed into the sink (renderer)
-
         // returning an animation renderer which handles clearing of the
         // viewport and computation of the fraction
         return new AnimationRenderer(pd) {
@@ -90,10 +77,7 @@ public class PullPipelineFactory {
 
                 rotationAngle += fraction;
 
-                filterViewTransform.setTransform(modelToViewTransform(pd, rotationAngle));
-
-
-                inputModel[0] = model;
+                filterViewTransform.setTransform(modelSpaceToViewSpace(pd, rotationAngle));
 
                 var graphics = pd.getGraphicsContext();
                 graphics.setStroke(pd.getModelColor());
@@ -101,10 +85,9 @@ public class PullPipelineFactory {
 
                 filterViewportTransform.pull().getFaces().forEach(f -> {
 
-                    Supplier<Stream<Vec4>> vertices = () -> Stream.of(f.getV1(), f.getV2(), f.getV3());
-                    var xs = vertices.get().mapToDouble(Vec4::getX).toArray();
-                    var ys = vertices.get().mapToDouble(Vec4::getY).toArray();
                     final int n = 3;
+                    var xs = new double[] { f.getV1().getX(), f.getV2().getX(), f.getV3().getX() };
+                    var ys = new double[] { f.getV1().getY(), f.getV2().getY(), f.getV3().getY() };
 
                     switch (pd.getRenderingMode()) {
                         case POINT:
@@ -122,16 +105,6 @@ public class PullPipelineFactory {
                     }
                 });
 
-
-
-
-                /*
-                model.getFaces().forEach(f -> {
-                    Stream.of(f.getV1(), f.getV2(), f.getV3()).forEach(vertex -> {
-                        pd.getGraphicsContext().fillOval(vertex.getX() * 50 + 200, 200-vertex.getY() * 50, 1, 1);
-                    });
-                });
-                */
             }
         };
     }
